9b3fe29a3e6f9fbe6370268dc602136d56b53f94
Michel Koolwaaij <michel.koolwaaij@han.nl>
2024-09-23 10:18:09 +0200

    Initial version with Spring Boot

---
+++ src/main/java/han/aim/se/noyoumaynot/NoyoumaynotApplication.java
@@ -0,0 +1,13 @@
+package han.aim.se.noyoumaynot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoyoumaynotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoyoumaynotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/han/aim/se/noyoumaynot/NoyoumaynotApplicationTests.java
@@ -0,0 +1,13 @@
+package han.aim.se.noyoumaynot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoyoumaynotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
